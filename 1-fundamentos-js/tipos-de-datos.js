02c2fcac768efe56036cc85f1e76baebf8083acf
Miguel <jesrous12@gmail.com>
2025-02-04 21:30:10 -0600

    agregué mis ejemplos de tipado

--- 1-fundamentos-js/tipos-de-datos.js
+++ 1-fundamentos-js/tipos-de-datos.js
@@ -17,3 +17,19 @@ console.log( "El tipo de dato es:",  typeof null )
 console.log( "El tipo de dato es:",  typeof undefined )
 
 console.log( "El tipo de dato es:",  typeof false )
+
+//Mis ejemplos
+
+console.log( "El tipo de dato es:",  typeof 78138473091839809813 )
+
+console.log( "El tipo de dato es:",  typeof 90.87 )
+
+console.log( "El tipo de dato es:",  typeof 'Hello, soy un tipo de dato de texto' )
+
+console.log( "El tipo de dato es:",  typeof '-0' )
+
+console.log( "El tipo de dato es:",  typeof -0 )
+
+
+
+
