39573ac8a5b2f6d77b0c83d32de27b03fd3d9dad
Miguel <jesrous12@gmail.com>
2025-02-04 20:52:59 -0600

    tipado de datos en JS

---
+++ 1-fundamentos-js/tipos-de-datos.js
@@ -0,0 +1,19 @@
+
+
+console.log( "El tipo de dato es:",  typeof 42 )
+
+console.log ( "El tipo de dato es:", typeof 'Veinticinco' )
+
+console.log( "El tipo de dato es:", typeof -666 )
+
+console.log( "El tipo de dato es:",  typeof true )
+
+console.log( "El tipo de dato es:",  typeof 0 )
+
+console.log( "El tipo de dato es:",  typeof '' )
+
+console.log( "El tipo de dato es:",  typeof null )
+
+console.log( "El tipo de dato es:",  typeof undefined )
+
+console.log( "El tipo de dato es:",  typeof false )
